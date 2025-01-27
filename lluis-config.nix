788741dc4a053d80c4ce55d197e7521eb1a8dde6
Luis Valero <lluisvalerodelai@gmail.com>
2025-01-27 10:00:30 +0100

    modular

---
+++ lluis-config.nix
@@ -0,0 +1,25 @@
+{config, pkgs, ...}:
+
+{
+	users.users.lluis = {
+		packages = with pkgs; [
+				cowsay
+				python3Full
+				libgcc
+				alacritty
+				tmux
+				git
+				gh
+				cmake
+				curlFull
+				zip
+				gnutar
+				docker
+				gnumake42
+				gnugrep
+				fzf
+				tree
+			];
+	};
+		
+}
